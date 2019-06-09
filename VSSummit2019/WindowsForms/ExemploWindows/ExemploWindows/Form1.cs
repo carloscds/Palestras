427d52d3878962698e47115aa4379b1a0bd67adc
Carlos dos Santos <cds@cds-software.com.br>
2019-06-09 19:57:08 -0300

    Exemplos VSSummit 2019

---
+++ VSSummit2019/WindowsForms/ExemploWindows/ExemploWindows/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dapper;
+
+namespace ExemploWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var conexao = new SqlConnection("data source=(local); initial catalog=northwind; integrated security=true;");
+            var customer = conexao.Query<Customer>("select customerID, companyName from customers");
+            dataGridView1.DataSource = customer;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+        }
+    }
+
+    public class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+}
